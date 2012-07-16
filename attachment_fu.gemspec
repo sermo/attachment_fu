59114b29ba2820824ca20b8fcdb27d88a2ceb5fe
Rusty Geldmacher <rgeldmacher@sermo.com>
2012-07-16 13:14:48 -0400

    Added a gemspec and a Rails engine

---
+++ attachment_fu.gemspec
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |s|
+  s.name          = 'attachment_fu'
+  s.version       = '1.0.0.sermo'
+  s.summary       = "Sermo fork of attachment_fu"
+  s.description   = "Sermo fork of attachment_fu"
+  s.authors       = ["technoweenie"]
+  s.email         = 'technoweenie@technoweenie.com'
+  s.homepage      = 'https://github.com/technoweenie/attachment_fu/'
+  s.files         = Dir['lib/**/*'] + Dir['vendor/**/*'] + Dir['test/**/*'] + ['README', 'amazon_s3.yml.tpl']
+  s.require_paths = ['lib']
+
+  s.add_development_dependency('rspec-rails')
+  s.add_development_dependency('rails', '3.0.10')
+  s.add_development_dependency('rspec', '~> 2.7.0')
+  s.add_development_dependency("mocha", "~> 0.9.7")
+end
---
+++ lib/attachment_fu.rb
@@ -0,0 +1,25 @@
+$:.unshift(File.expand_path('../../vendor', __FILE__))
+require 'rails'
+require 'tempfile'
+
+Tempfile.class_eval do
+  # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
+  def make_tmpname(basename, n)
+    ext = nil
+    sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
+  end
+end
+
+require 'geometry'
+
+module AttachmentFu
+  class Engine < Rails::Engine
+    config.autoload_paths << File.expand_path("..", __FILE__)
+    initializer 'attachment_fu' do
+      require 'technoweenie/attachment_fu'
+      ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
+      Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
+      FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
+    end
+  end
+end
